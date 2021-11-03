b7d133536ad885d792836601bec0eacd5a91659f
Nao Pross <np@0hm.ch>
2021-11-03 18:10:16 +0100

    Delete demo.py and and comments to sketch.py

--- src/sketch.py
+++ src/sketch.py
@@ -1,11 +1,19 @@
+#!/usr/bin/env python3
+
 from dearpygui.dearpygui import *
 from dearpygui.demo import show_demo
 
+# Create GL context and initialize DearPyGUI
 create_context()
 create_viewport()
 setup_dearpygui()
 
+# Show demo for dev
 show_demo()
+
+
+#================================================
+# GUI Callback functions
 
 def _on_params_close():
     pass
@@ -18,6 +26,9 @@ def _on_rx_node_link(sender, app_data):
 def _on_rx_node_delink(sender, app_data):
     link_id = app_data
     delete_item(link_id)
+
+#================================================
+# Flow Graph Window
 
 with window(label="RX DSP Flow Graph", width=800, height=800, on_close=_on_params_close, pos=(100,100), tag="rx_win"):
     with node_editor(callback=_on_rx_node_link, delink_callback=_on_rx_node_delink):
@@ -55,6 +66,8 @@ with window(label="RX DSP Flow Graph", width=800, height=800, on_close=_on_param
         add_node_link(get_alias_id("eq_out"), get_alias_id("pll_in"))
 
 
+#================================================
+# Start window and main loop
 
 show_viewport()
 start_dearpygui()
