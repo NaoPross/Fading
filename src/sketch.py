8733e3563306a193962d1f87421a2b817eca28f8
sara <sara.halter@gmx.ch>
2021-11-06 17:58:23 +0100

    An scr gearbeitet

--- src/sketch.py
+++ src/sketch.py
@@ -3,7 +3,7 @@
 from dearpygui.dearpygui import *
 from dearpygui.demo import show_demo
 
-import qpks
+import qpks as qp
 
 # Create GL context and initialize DearPyGUI
 create_context()
@@ -32,11 +32,62 @@ def _on_rx_node_delink(sender, app_data):
 #================================================
 # Flow Graph Window
 
-with window(label="RX DSP Flow Graph", width=800, height=800, on_close=_on_params_close, pos=(100,100), tag="rx_win"):
+# with window(label="RX DSP Flow Graph", width=800, height=800, on_close=_on_params_close, pos=(100,100), tag="rx_win"):
+#     with node_editor(callback=_on_rx_node_link, delink_callback=_on_rx_node_delink):
+#         with node(label="USRP Source", pos=(20,100)):
+#             with node_attribute(tag="src_out", attribute_type=mvNode_Attr_Output):
+#                 add_text("Signal from antenna")
+
+#         with node(label="Clock Sync", pos=(200,200)):
+#             with node_attribute(tag="clksync_in", attribute_type=mvNode_Attr_Input):
+#                 add_text("Input")
+
+#             with node_attribute(tag="clksync_out", attribute_type=mvNode_Attr_Output):
+#                 add_text("Synchronized")
+
+#         with node(label="Equalizer", pos=(350,100)):
+#             with node_attribute(attribute_type=mvNode_Attr_Static):
+#                 add_input_float(label="Gain", width=150)
+
+#             with node_attribute(tag="eq_in", attribute_type=mvNode_Attr_Input):
+#                 add_text("Input")
+
+#             with node_attribute(tag="eq_out", attribute_type=mvNode_Attr_Output):
+#                 add_text("Equalized")
+
+#         with node(label="Phase Locked Loop", pos=(600, 200)):
+#             with node_attribute(tag="pll_in", attribute_type=mvNode_Attr_Input):
+#                 add_text("Input")
+
+#             with node_attribute(tag="pll_out", attribute_type=mvNode_Attr_Output):
+#                 add_text("Locked")
+
+
+#         add_node_link(get_alias_id("src_out"), get_alias_id("clksync_in"))
+#         add_node_link(get_alias_id("clksync_out"), get_alias_id("eq_in"))
+#         add_node_link(get_alias_id("eq_out"), get_alias_id("pll_in"))
+
+
+
+with window(label="Example Window"):
+    add_text("Hello world")
+
+#================================================
+# QPSK Window
+
+q = qp.qpsk_nogui() # Klasse initalisieren !!!
+
+with window(label="QPSK", width=800, height=800, on_close=_on_params_close, pos=(100,100), tag="rx_win"):
     with node_editor(callback=_on_rx_node_link, delink_callback=_on_rx_node_delink):
-        with node(label="USRP Source", pos=(20,100)):
+        
+        with node(label="USRP Source (Random Source)", pos=(20,100)):
             with node_attribute(tag="src_out", attribute_type=mvNode_Attr_Output):
                 add_text("Signal from antenna")
+                print(q.analog_random_source_x_0)
+                add_text(q.analog_random_source_x_0)
+                add_text(qp.test)
+                add_text(q.test_2(1))
+
 
         with node(label="Clock Sync", pos=(200,200)):
             with node_attribute(tag="clksync_in", attribute_type=mvNode_Attr_Input):
@@ -67,14 +118,13 @@ with window(label="RX DSP Flow Graph", width=800, height=800, on_close=_on_param
         add_node_link(get_alias_id("clksync_out"), get_alias_id("eq_in"))
         add_node_link(get_alias_id("eq_out"), get_alias_id("pll_in"))
 
-
-<<<<<<< HEAD
-with window(label="Example Window"):
+#================================================
+# Plot Window
+with window(label="Plots"):
     add_text("Hello world")
-=======
 #================================================
 # Start window and main loop
->>>>>>> origin/master
+
 
 show_viewport()
 start_dearpygui()
