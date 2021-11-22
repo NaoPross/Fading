02d557270749fcadfce2a75ab70ac169adbaf90f
Nao Pross <np@0hm.ch>
2021-11-22 19:00:35 +0100

    Make install.sh run QA

--- src/gr-fadingui/install.sh
+++ src/gr-fadingui/install.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+set -o errexit
+
 mkdir -p build
 cmake -DGR_PYTHON_DIR=/usr/lib/python3/dist-packages/ -GNinja -Bbuild
 ninja -C build
+
+# run QA
+export CTEST_OUTPUT_ON_FAILURE=1
+ninja -C build test
+cat build/Testing/Temporary/LastTest.log
 
 # install to system
 sudo ninja -C build install
